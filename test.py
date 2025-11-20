3d748b03e28c0db554e0bc3a1b3d98956da3fb91
Ananthu Thulaseedharan Pillai Jayakumari <ananthu.thulaseedharan.pillai.jayakumari@gds.ey.com>
2025-11-20 10:42:59 +0530

    Updated code 19th Nov

--- test.py
+++ test.py
@@ -2,19 +2,15 @@ import streamlit as st
 import base64
 
 
-# st.markdown("""
-#     <style>
-#     .stToolbarActionButton {
-#         display: none !important;    
-#     }
-#     footer {display: none}
+st.markdown("""
+    <style>
+    .stToolbarActionButton {
+        display: none !important;    
+    }
+    footer {display: none}
 
-#     </style>
-#     """, unsafe_allow_html=True)
-
-hide_streamlit_style = """ <style> #MainMenu {visibility: hidden;} footer {visibility: hidden;} </style> """
-
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    </style>
+    """, unsafe_allow_html=True)
 
 st.set_page_config(page_title="Data Flow Lineage Visualizer", layout="wide")
 
